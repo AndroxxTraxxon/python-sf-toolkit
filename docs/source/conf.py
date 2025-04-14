01097a11ff531549951598416760d89815a4720b
David Culbreth <david.culbreth.256@gmail.com>
2025-04-14 09:23:10 -0500

    made a logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,11 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "sf_toolkit.png"
+# html_theme_options = {
+#     'logo_only': False,
+#     'display_version': True,
+# }
 
 # -- Options for autodoc -----------------------------------------------------
 autodoc_member_order = 'bysource'
