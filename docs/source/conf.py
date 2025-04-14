76a6f7ced623c9a1d2a7b897514742e26ae98122
David Culbreth <david.culbreth.256@gmail.com>
2025-04-13 22:30:35 -0500

    more missing docs pieces

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Salesforce Toolkit'
-copyright = '2024'
+copyright = '2025'
 author = 'David Culbreth'
 release = '0.1.0'
 
